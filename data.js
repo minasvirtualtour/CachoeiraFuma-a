e0c9137a7ddee92e4c879ed9fcf53c1597d3ea0a
minasvirtualtour <149280511+minasvirtualtour@users.noreply.github.com>
2023-11-24 22:28:45 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,227 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-incio",
+      "name": "Início",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.0765077904760183,
+        "pitch": -0.2653636056011326,
+        "fov": 1.2038499345169202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9347041527364128,
+          "pitch": 0.24781514824490358,
+          "rotation": 0,
+          "target": "1-trilha2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2912515904548112,
+          "pitch": -0.1428583079984289,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Estacionamento</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Existe um espaço reservado para o estacionamento de carros no início da trilha, facilitando o acesso as cachoeiras</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1-trilha2",
+      "name": "Trilha2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 1.3367380967327236,
+        "pitch": 0.04219625432592622,
+        "fov": 1.2038499345169202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.787380701519627,
+          "pitch": 0.09686457840053464,
+          "rotation": 0,
+          "target": "4-cachoeira-principal"
+        },
+        {
+          "yaw": -1.7758770325315023,
+          "pitch": 0.016492762040765996,
+          "rotation": 0,
+          "target": "2-trilha3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-trilha3",
+      "name": "Trilha3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.12551808103644824,
+        "pitch": -0.05096819757266147,
+        "fov": 1.2038499345169202
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6323958386679323,
+          "pitch": 0.15606029097845742,
+          "rotation": 0,
+          "target": "3-mini-cachoeira"
+        },
+        {
+          "yaw": 1.4968100686083563,
+          "pitch": -0.021462573369591453,
+          "rotation": 0,
+          "target": "1-trilha2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.20729389150424637,
+          "pitch": -0.08814284203459266,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Cachoeira</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Aqui tem uma visão de uma das cachoeira do complexo da Fumaça.</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "3-mini-cachoeira",
+      "name": "Mini cachoeira",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.16319390233456232,
+        "pitch": -0.016473287573827378,
+        "fov": 1.2038499345169202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0193774254457333,
+          "pitch": -0.0021127631390580603,
+          "rotation": 5.497787143782138,
+          "target": "2-trilha3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3552412756812853,
+          "pitch": 0.26951086730998597,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Cachoeira Baixa da Fumaça</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Acesso atras de uma trilha, no encontro de dois rios</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "4-cachoeira-principal",
+      "name": "Cachoeira principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.03472629080980383,
+        "pitch": -0.12774698235409154,
+        "fov": 1.2038499345169202
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6195910165457583,
+          "pitch": 0.010960637008516017,
+          "rotation": 0,
+          "target": "2-trilha3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.368417392917447,
+          "pitch": 0.15316506223279802,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Atenção</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">A cachoeira da Fumaça está interditada para banhos</font></font>"
+        },
+        {
+          "yaw": -2.498395675765014,
+          "pitch": 0.14095549256754758,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Véu de Noiva</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Trilha de acesso para a cachoeira véu de noiva, pode seguir o caminho para ter a visao de cima ou potar por ir por baixo.</font></font>"
+        }
+      ]
+    }
+  ],
+  "name": "Tour Cachoeira Fumaça",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
